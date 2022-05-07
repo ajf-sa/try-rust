2031f1794dfe72cb7d666a7b69818c25b49de99d
alfuhigi <abajall@gmail.com>
2022-05-07 12:03:57 +0300

    ..

--- v006-string/src/main.rs
+++ v006-string/src/main.rs
@@ -7,4 +7,22 @@ fn main() {
     println!("his length name is {}", uname.len());
     let fname :String = String::from(name);
     println!("his length name is {}", fname.len());
+
+    let mut string = String::new();
+    string.push_str("Hello");
+    string.push_str(" World");
+    println!("{}", string);
+
+    let name3 = "hello there , how are you today ?";
+    println!("{}", name3);
+
+    let name4 = name3.replace("hello", "Hello");
+    println!("{}", name4);
+
+    let mut company = "Tutorial".to_string();
+    company.push('s');
+    println!("{}",company);
+
+    let  fulname = " ahmed ahmed ahmed  ";
+    println!("{}",fulname.trim());
 }
