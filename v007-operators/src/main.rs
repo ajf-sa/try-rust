fbd2ee757a20bedae5c4f497dc2e35bcbd522784
alfuhigi <abajall@gmail.com>
2022-05-07 12:04:25 +0300

    >

---
+++ v007-operators/src/main.rs
@@ -0,0 +1,18 @@
+fn main() {
+println!("{}",(1+5));
+println!("{}",(1-5));
+println!("{}",(1*5));
+println!("{}",(1/5));
+println!("{}",(1%5));
+println!("{}",(1^6));
+println!("{}",(1&6));
+println!("{}",(1|6));
+println!("{}",(1<<6));
+println!("{}",(1>>6));
+println!("{}",(1==6));
+println!("{}",(1!=6));
+println!("{}",(1<6));
+println!("{}",(1>6));
+println!("{}",(1<=6));
+println!("{}",(1>=6));
+}
