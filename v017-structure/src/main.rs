5dd2a6115760345abd25d52b1f3c9247c7a2ee1c
abdulaziz at work <abajall@gmail.com>
2022-05-11 16:32:45 +0300

    .

--- v017-structure/src/main.rs
+++ v017-structure/src/main.rs
@@ -9,7 +9,25 @@ fn main() {
         name:String::from("alfuhigi"),
         age:99
     };
-
-    println!("Name is :{} company is {} age is {}",emp1.name,emp1.company,emp1.age);
+    let emp2 = Employee{
+        company:String::from("aramco"),
+        name:String::from("ali"),
+        age:102
+    };
+    
+let elder = who_is_elder(emp1,emp2);
+println!("elder is:");
+  display(elder);
    
+}
+fn display( emp:Employee) {
+   println!("Name is :{} company is {} age is {}",emp.name,emp.company,emp.age);
+}
+
+fn who_is_elder (emp1:Employee,emp2:Employee)->Employee {
+   if emp1.age>emp2.age {
+      return emp1;
+   } else {
+      return emp2;
+   }
 }
