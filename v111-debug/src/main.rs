dde4b26725debed2974b7b08a3378e0b88cf899f
alfuhigi <abajall@gmail.com>
2022-05-13 16:07:23 +0300

    struct

---
+++ v111-debug/src/main.rs
@@ -0,0 +1,9 @@
+fn main() {
+    eprintln!("Hello, world!");
+}
+
+#[test]
+fn test_main() {
+    let cat = Some("Cat");
+    dbg!(cat);
+}
---
+++ v112-struct/src/main.rs
@@ -0,0 +1,19 @@
+use fake::faker::internet::raw::FreeEmail;
+use fake::faker::phone_number::raw::PhoneNumber;
+use fake::locales::EN;
+use fake::{Dummy, Fake, Faker};
+
+#[derive(Dummy)]
+struct Authorize {
+    id: i32,
+    email: String,
+    phone: String,
+    password: String,
+}
+
+fn main() {
+    let mut f: Authorize = Faker.fake();
+    f.email = FreeEmail(EN).fake();
+    f.phone = PhoneNumber(EN).fake();
+    println!("{} {} {} {}", f.id, f.email, f.phone, f.password);
+}
