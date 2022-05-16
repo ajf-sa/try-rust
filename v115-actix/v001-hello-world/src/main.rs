901af86d12ac5083bf638f3032e35dd5f8583353
alfuhigi <abajall@gmail.com>
2022-05-16 20:48:56 +0300

    actix

--- v115-actix/v001-hello-world/src/main.rs
+++ v115-actix/v001-hello-world/src/main.rs
@@ -1,8 +1,8 @@
-use actix_web::{get, web, App, HttpResponse, HttpServer, Responder};
+use actix_web::{get, App, HttpResponse, HttpServer, Responder};
 
 #[actix_web::main]
 async fn main() -> std::io::Result<()> {
-    HttpServer::new(|| App::new().service(index).service(index2))
+    HttpServer::new(|| App::new().service(index))
         .bind(("127.0.0.1", 8081))?
         .run()
         .await
@@ -11,9 +11,4 @@ async fn main() -> std::io::Result<()> {
 #[get("/")]
 async fn index() -> impl Responder {
     HttpResponse::Ok().body("Hello world!")
-}
-
-#[get("/{name}")]
-async fn index2(name: web::Path<String>) -> impl Responder {
-    HttpResponse::Ok().body(format!("Hello {}!", name))
 }
