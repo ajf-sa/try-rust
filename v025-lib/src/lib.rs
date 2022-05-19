ecfdcf41fc4a4b014227162bf7acf77b1986fdb4
abdulaziz at work <abajall@gmail.com>
2022-05-19 11:10:30 +0300

    lib

--- v025-lib/src/lib.rs
+++ v025-lib/src/lib.rs
@@ -1,8 +1,29 @@
+struct MyIterWrapper<'a,T>{
+    slice:&'a[T]
+}
+
+impl<'a,T> Iterator for MyIterWrapper <'a,T>{
+
+    type Item = &'a T;
+
+    fn next(&mut self) -> Option<Self::Item>{
+   let elemnt = self.slice.get(0) ;
+   todo!()
+    }
+}
 #[cfg(test)]
 mod tests {
+    use crate::MyIterWrapper;
+
     #[test]
     fn it_works() {
-        let result = 2 + 2;
-        assert_eq!(result, 4);
+     let mut collection = vec![1,2,3,4,5,6,7] ;
+        let wrapper = MyIterWrapper{
+            slice: &collection[..],
+        };
+     for c in wrapper {
+
+     
+     }
     }
 }
