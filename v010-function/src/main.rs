e91c072c2fafb5386e20fd2ea1859d8753e60ea9
alfuhigi <abajall@gmail.com>
2022-05-20 16:21:05 +0300

    .

--- v010-function/src/main.rs
+++ v010-function/src/main.rs
@@ -1,27 +1,32 @@
 fn main() {
-// calling a function
- hello();  
- let hi = say_hello();
- println!("I will say {}",hi);
- let n = add(4,5);
- println!("sum is {}",n);
- let mut no:i32 = 5;
- to_zero(&mut no);
- println!("The value of no is:{}",no);
+    // calling a function
+    hello();
+    let hi = say_hello();
+    println!("I will say {}", hi);
+    let n = add(4, 5);
+    println!("sum is {}", n);
+    let mut no: i32 = 5;
+    // to_zero(&mut no);
+    back_fn(&mut no);
+    println!("The value of no is:{}", no);
 }
 // defining function
-fn hello(){
+fn hello() {
     println!("this is first function on rust");
 }
 
-fn say_hello() -> String{
+fn say_hello() -> String {
     "HI".to_string()
 }
 
-fn add(x:i32,y :i32) -> i32 {
-    x+y
+fn add(x: i32, y: i32) -> i32 {
+    x + y
 }
 
-fn to_zero(p:&mut i32) {
+fn to_zero(p: &mut i32) {
     *p = 0;
+}
+
+fn back_fn(s: &mut i32) -> fn(&mut i32) {
+    to_zero
 }
