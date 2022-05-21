9c23d3b90b08646d1ea61eba4901349310e84721
alfuhigi <abajall@gmail.com>
2022-05-21 11:08:00 +0300

    guess game

--- v101-guess-game/src/main.rs
+++ v101-guess-game/src/main.rs
@@ -1,10 +1,40 @@
-use std::io;
+use rand::random;
+use std::cmp::Ordering;
+use std::io::stdin;
 fn main() {
     println!("Guess the number!");
-    println!("Please input your guess.");
-    let mut guess = String::new();
-    io::stdin()
-        .read_line(&mut guess)
-        .expect("Failed to read line");
-    println!("You guessed: {}", guess);
+
+    let secret_number = (random::<u32>() % 100) + 1;
+    loop {
+        let mut number = String::from("");
+        println!("Please input your guess.");
+        let _ = stdin().read_line(&mut number).unwrap();
+        let input_number: Result<u32, _> = number.trim().parse();
+        let num = match input_number {
+            Ok(num) => num,
+            Err(_) => {
+                println!("Please input a number!");
+                continue;
+            }
+        };
+
+        println!("You guessed: {}", num);
+        match cmp(num, secret_number) {
+            Ordering::Less => println!("Too small!"),
+            Ordering::Greater => println!("Too big!"),
+            Ordering::Equal => {
+                println!("You win!");
+                return;
+            }
+        }
+    }
+}
+fn cmp(a: u32, b: u32) -> Ordering {
+    if a < b {
+        Ordering::Less
+    } else if a > b {
+        Ordering::Greater
+    } else {
+        Ordering::Equal
+    }
 }
