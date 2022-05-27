aefe84645567f0d800d1485622dd7f0e396a082a
alfuhigi <abajall@gmail.com>
2022-05-27 13:31:43 +0300

    struct

--- v112-struct/src/main.rs
+++ v112-struct/src/main.rs
@@ -1,21 +1,65 @@
-use fake::faker::internet::raw::FreeEmail;
-use fake::faker::phone_number::raw::PhoneNumber;
-use fake::locales::EN;
-use fake::{Dummy, Fake, Faker};
+struct Persion {
+    name: String,
+    job: Option<Job>,
+}
 
-#[derive(Dummy)]
-struct Authorize {
-    id: i32,
-    email: String,
-    phone: String,
-    password: String,
+struct Job {
+    position: String,
+    phone: Option<Phone>,
+}
+
+struct Phone {
+    default: bool,
+    number: Option<u32>,
+}
+
+impl Persion {
+    fn persion_name(&self) -> String {
+        self.name.clone()
+    }
+    fn job_position(&self) -> String {
+        match &self.job {
+            Some(job) => job.position.clone(),
+            None => String::from(""),
+        }
+    }
+    fn phone(&self) -> u32 {
+        self.job
+            .as_ref()
+            .unwrap()
+            .phone
+            .as_ref()
+            .unwrap()
+            .number
+            .unwrap()
+    }
+    fn job(&self) -> String {
+        self.job
+            .as_ref()
+            .unwrap()
+            .phone
+            .as_ref()
+            .unwrap()
+            .number
+            .unwrap()
+            .to_string()
+    }
 }
 
 fn main() {
-    let mut f: Authorize = Faker.fake();
-    f.email = FreeEmail(EN).fake();
-    f.phone = PhoneNumber(EN).fake();
-    println!("{} {} {} {}", f.id, f.email, f.phone, f.password);
-    let mut space = "  ";
-    space = space.len().to_string().as_str();
+    let p = Persion {
+        job: Some(Job {
+            phone: Some(Phone {
+                number: Some(123456789),
+                default: true,
+            }),
+            position: "Software Engineer".to_string(),
+        }),
+        name: String::from("John"),
+    };
+
+    println!("{}", p.phone());
+    println!("{}", p.job());
+    println!("{}", p.job_position());
+    println!("{}", p.persion_name());
 }
