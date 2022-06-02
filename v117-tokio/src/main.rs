068773c6202c3f03b4da45557af219b6203f1f26
alfuhigi <abajall@gmail.com>
2022-06-02 11:25:18 +0300

    change more tokio

--- v117-tokio/src/main.rs
+++ v117-tokio/src/main.rs
@@ -1,6 +1,6 @@
 use tokio::{
     io::{AsyncReadExt, AsyncWriteExt},
-    net::TcpListener,
+    net::{TcpListener, TcpStream},
 };
 
 #[tokio::main]
@@ -8,29 +8,29 @@ async fn main() -> Result<(), Box<dyn std::error::Error>> {
     let listener = TcpListener::bind("0.0.0.0:8081").await?;
     loop {
         let (mut socket, addr) = listener.accept().await?;
-        tokio::spawn(async move {
-            let mut buf = [0; 1024];
+        handle(socket).await;
+    }
+}
 
-            loop {
-                let n = match socket.read(&mut buf).await {
-                    // socket closed
-                    Ok(n) if n == 0 => return,
-                    Ok(n) => n,
-                    Err(e) => {
-                        eprintln!("failed to read from socket; err = {:?}", e);
-                        return;
-                    }
-                };
+async fn handle(mut strem: TcpStream) -> Result<(), Box<dyn std::error::Error>> {
+    tokio::spawn(async move {
+        let mut buf = vec![0; 1024];
 
-                let cmd = std::str::from_utf8(&buf[0..n]).unwrap();
-                println!("{}", addr.ip());
-                println!("{}", cmd);
-
-                if let Err(e) = socket.write_all(&buf[0..n]).await {
-                    eprintln!("failed to write to socket; err = {:?}", e);
+        loop {
+            let n = match strem.read(&mut buf).await {
+                Ok(n) if n == 0 => return,
+                Ok(n) => n,
+                Err(e) => {
+                    eprintln!("failed to read from socket; err = {:?}", e);
                     return;
                 }
-            }
-        });
-    }
+            };
+
+            let cmd = std::str::from_utf8(&buf[0..n]).unwrap();
+            println!("{}", cmd);
+
+            strem.write_all(&buf[0..n]).await;
+        }
+    });
+    Ok(())
 }
