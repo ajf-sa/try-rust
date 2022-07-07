14153508b1876412f12b9226ec5014be4d79df96
alfuhigi <abajall@gmail.com>
2022-07-07 23:29:08 +0300

    add regex

--- v117-tokio/src/main.rs
+++ v117-tokio/src/main.rs
@@ -1,8 +1,8 @@
-use response::{Response, status};
-use tokio::net::{ TcpListener};
-use tokio::io::{ AsyncReadExt};
 use regex::Regex;
+use response::{status, Response};
 use std::error::Error;
+use tokio::io::AsyncReadExt;
+use tokio::net::TcpListener;
 mod response;
 
 pub enum Method {
@@ -13,76 +13,63 @@ pub enum Method {
 }
 
 #[tokio::main]
-async fn main() -> Result<(),Box<dyn Error>> {
-   let  l = TcpListener::bind("127.0.0.1:8082").await?;
-   loop {
-   let (mut s,_) = l.accept().await?;
-   let mut buf = [0; 1024];
+async fn main() -> Result<(), Box<dyn Error>> {
+    let l = TcpListener::bind("127.0.0.1:8082").await?;
+    loop {
+        let (mut s, _) = l.accept().await?;
+        let mut buf = [0; 1024];
 
-  match s.read(&mut buf).await {
-      Ok(count) if count == 0 => { println!("client closed connection"); break; },
-      Ok(count) => {
-        let req = std::str::from_utf8(&buf[0..count]).unwrap().split("\r\n").collect::<Vec<&str>>();
-        let parts = req[0].split(" ").collect::<Vec<&str>>();
-        if parts.len() < 2 {
-            let mut res = Response::new(s);
-            res.sendfile(400,status(400), "static/__400.html").await;
-           return Ok(());
-        }
-       
-        match (parts[0], parts[1]) {
-         
-            ("GET",path) => {
-                // match path {
-                //     "/" => {
-                //         let mut res = Response::new(s);
-                //         res.sendfile(200, status(200), "static/index.html").await;
-
-                //     }
-                //     "/blog" =>{
-                //         let mut res = Response::new(s);
-                //         res.sendfile(200, status(200), "static/blog/index.html").await; 
-                //     }
-                //     "" => {
-                        
-                //     },
-                //     _ =>{
-                //         let mut res = Response::new(s);
-                //         res.sendfile(200, status(200), "static/__404.html").await;
-                //     }
-                // }
-                let mut res = Response::new(s);
-                let r = Regex::new("^/$").unwrap();
-                if r.is_match(path) {
-                    res.sendfile(200, status(200), "static/index.html").await; 
-                }
-
-               let r = Regex::new("^/blog$").unwrap();
-               if r.is_match(path) {
-                res.sendfile(200, status(200), "static/blog/index.html").await;   
-                }
-                
-                let r = Regex::new("^/blog/([0-9a-zA-Z]{1,30})$").unwrap();
-                if r.is_match(path) {
-                    println!("{}",path[6..].to_string());
-                     res.sendfile(200, status(200), "static/blog/index.html").await;  
-                }
-               res.sendfile(200, status(200), "static/__404.html").await; 
-               
+        match s.read(&mut buf).await {
+            Ok(count) if count == 0 => {
+                println!("client closed connection");
+                break;
             }
-            _ => {
-                let mut res = Response::new(s);
-                res.sendfile(200, status(200), "static/__404.html").await;
-               
-            }
-        }
-       
-      },
-      Err(err) => { println!("error: {}", err); break; },
+            Ok(count) => {
+                let req = std::str::from_utf8(&buf[0..count])
+                    .unwrap()
+                    .split("\r\n")
+                    .collect::<Vec<&str>>();
+                let parts = req[0].split(" ").collect::<Vec<&str>>();
+                if parts.len() < 2 {
+                    let mut res = Response::new(s);
+                    res.sendfile(400, status(400), "static/__400.html").await;
+                    return Ok(());
+                }
 
-  };
- }
- Ok(())
+                match (parts[0], parts[1]) {
+                    ("GET", path) => {
+                        let mut res = Response::new(s);
+                        let r = Regex::new("^/$").unwrap();
+                        if r.is_match(path) {
+                            res.sendfile(200, status(200), "static/index.html").await;
+                        }
+
+                        let r = Regex::new("^/blog$").unwrap();
+                        if r.is_match(path) {
+                            res.sendfile(200, status(200), "static/blog/index.html")
+                                .await;
+                        }
+
+                        let r = Regex::new("^/blog/([0-9a-zA-Z]{1,30})$").unwrap();
+                        if r.is_match(path) {
+                            println!("{}", path[6..].to_string());
+                            res.sendfile(200, status(200), "static/blog/index.html")
+                                .await;
+                        }
+
+                        res.sendfile(200, status(200), "static/__404.html").await;
+                    }
+                    _ => {
+                        let mut res = Response::new(s);
+                        res.sendfile(200, status(200), "static/__404.html").await;
+                    }
+                }
+            }
+            Err(err) => {
+                println!("error: {}", err);
+                break;
+            }
+        };
+    }
+    Ok(())
 }
-
-
--- v117-tokio/src/response.rs
+++ v117-tokio/src/response.rs
@@ -1,7 +1,7 @@
 use tokio::fs::File;
-use tokio::io::{BufWriter, AsyncWriteExt, AsyncReadExt};
-use tokio::io::{BufReader};
-use tokio::io::{Result};
+use tokio::io::BufReader;
+use tokio::io::Result;
+use tokio::io::{AsyncReadExt, AsyncWriteExt, BufWriter};
 use tokio::net::TcpStream;
 
 pub struct Response {
@@ -23,21 +23,24 @@ impl Response {
             writer: BufWriter::new(client),
         }
     }
-     pub async fn write_status(&mut self, code: i32, status: &str) -> Result<usize> {
+    pub async fn write_status(&mut self, code: i32, status: &str) -> Result<usize> {
         self.writer
-            .write(format!("HTTP/1.1 {} {}\r\n", code, status).as_bytes()).await
+            .write(format!("HTTP/1.1 {} {}\r\n", code, status).as_bytes())
+            .await
     }
     pub async fn write_header(&mut self, key: &str, val: &str) -> Result<usize> {
         self.writer
-            .write(format!("{}: {}\r\n", key, val).as_bytes()).await
+            .write(format!("{}: {}\r\n", key, val).as_bytes())
+            .await
     }
 
     pub async fn write_body(&mut self, val: &[u8]) -> Result<usize> {
-        self.write_header("Content-Length", val.len().to_string().as_str()).await?;
+        self.write_header("Content-Length", val.len().to_string().as_str())
+            .await?;
         self.writer.write(b"\n").await?;
         self.writer.write(val).await
     }
-    pub  fn mine_type(&self, key: &str) -> &str {
+    pub fn mine_type(&self, key: &str) -> &str {
         if let Some((_, ext)) = key.rsplit_once(".") {
             match ext {
                 "html" => "text/html",
@@ -57,15 +60,17 @@ impl Response {
 
     pub async fn write_file(&mut self, mut path: &str) -> Result<usize> {
         self.write_header("Connection", "Keep-Alive").await?;
-        self.write_header("Keep-Alive", "timeout=5, max=100").await?;
-         self.write_header(
+        self.write_header("Keep-Alive", "timeout=5, max=100")
+            .await?;
+        self.write_header(
             "Content-Type",
             format!("{}; charset=utf-8", self.mine_type(path)).as_str(),
-        ).await?;
+        )
+        .await?;
         if path.starts_with("/") {
             path = &path[1..];
         }
-        let file = match File::open(path).await{
+        let file = match File::open(path).await {
             Ok(file) => file,
             Err(e) => {
                 println!("{}:{}", path, e);
@@ -76,8 +81,6 @@ impl Response {
         let mut reader = BufReader::new(file);
         reader.read_to_end(&mut buf).await?;
 
-       
-    
         self.write_body(&buf).await
     }
 
