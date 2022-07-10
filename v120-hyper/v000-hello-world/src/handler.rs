36cdc3cbb1a32d4f534c55b8cfdee69c35c267ee
alfuhigi <abajall@gmail.com>
2022-07-10 18:25:55 +0300

    add protect

--- v120-hyper/v000-hello-world/src/handler.rs
+++ v120-hyper/v000-hello-world/src/handler.rs
@@ -1,26 +1,53 @@
-use std::{convert::Infallible, io};
+use std::io;
 
-use hyper::{Request, Body, Response, header::{HeaderValue, CONTENT_LENGTH, CONTENT_TYPE}, StatusCode};
+use hyper::{
+    header::{CONTENT_LENGTH, CONTENT_TYPE, self, HeaderValue},
+    Body, Request, Response, StatusCode,
+};
 use routerify::ext::RequestExt;
 
 pub async fn index(_r: Request<Body>) -> Result<Response<Body>, io::Error> {
-   Ok(make_response(&"index", StatusCode::OK))
- }
+    Ok(make_response(&"index", StatusCode::OK))
+}
 
- pub async fn blog(r: Request<Body>) -> Result<Response<Body>, io::Error> {
-   let id = match r.param("id") {
-       Some(x) => x,
-       None => "0",
-   }; 
-   println!("{id}");
-   Ok(make_response(&"blog", StatusCode::OK)) 
- }
+pub async fn protect_page(_r: Request<Body>) -> Result<Response<Body>, io::Error> {
+    Ok(make_response(&"protect", StatusCode::OK))
+}
 
- fn make_response(body: &'static str, status: StatusCode) -> Response<Body> {
-   Response::builder()
-       .status(status)
-       .header(CONTENT_LENGTH, body.len() as u64)
-       .header(CONTENT_TYPE, "text/plain")
-       .body(Body::from(body))
-       .expect("Failed to construct response")
+pub async fn login(_r: Request<Body>) -> Result<Response<Body>, io::Error> {
+   let mut res = Response::<Body>::default();
+   res.headers_mut()
+   .insert(header::SET_COOKIE, HeaderValue::from_str("id=admin;").unwrap());
+    Ok(res)
+}
+
+pub async fn blog(r: Request<Body>) -> Result<Response<Body>, io::Error> {
+    let id = match r.param("id") {
+        Some(x) => x,
+        None => "0",
+    };
+    println!("{id}");
+    let body = format!(
+        "
+   <html>
+   <head>
+   <title>{}</title>
+   </head>
+   </html>
+   ",
+        id
+    );
+    Ok(make_response(
+        Box::leak(body.into_boxed_str()),
+        StatusCode::OK,
+    ))
+}
+
+fn make_response(body: &'static str, status: StatusCode) -> Response<Body> {
+    Response::builder()
+        .status(status)
+        .header(CONTENT_LENGTH, body.len() as u64)
+        .header(CONTENT_TYPE, "text/html; charset=utf-8")
+        .body(Body::from(body))
+        .expect("Failed to construct response")
 }
--- v120-hyper/v000-hello-world/src/main.rs
+++ v120-hyper/v000-hello-world/src/main.rs
@@ -1,34 +1,70 @@
-use std::{net::SocketAddr, convert::Infallible, io};
+use std::{convert::Infallible, io, net::SocketAddr};
 
-use hyper::{service::{make_service_fn, service_fn}, Response, Body, Request, Server,  Method};
+use hyper::{
+    service::{make_service_fn, service_fn},
+    Body, Method, Request, Response, Server, body, header,
+};
 
 mod handler;
-use handler::{index, blog};
-use routerify::{Router, ext::RequestExt, Middleware, RouterService};
+use handler::{blog, index, protect_page, login};
+use routerify::{ext::RequestExt, Middleware, Router, RouterService, RequestInfo};
 #[tokio::main(flavor = "current_thread")]
 async fn main() {
-   let router = router();
-   let service = RouterService::new(router).unwrap();
-   let addr = SocketAddr::from(([0,0,0,0],8082));
-   let server = Server::bind(&addr).serve(service);
-   if let Err(e) = server.await {
-      eprintln!("server error: {}", e);
-  }
+    let router = router();
+    let service = RouterService::new(router).unwrap();
+    let addr = SocketAddr::from(([0, 0, 0, 0], 8082));
+    let server = Server::bind(&addr).serve(service);
+    if let Err(e) = server.await {
+        eprintln!("server error: {}", e);
+    }
 }
 
-
-
 async fn logger_middleware(req: Request<Body>) -> Result<Request<Body>, io::Error> {
-   println!("{} {} {}", req.remote_addr(), req.method(), req.uri().path());
-   Ok(req)
+    println!(
+        "{} {} {}",
+        req.remote_addr(),
+        req.method(),
+        req.uri().path()
+    );
+    Ok(req)
+}
+
+async fn protect(mut res: Response<Body>, req_info: RequestInfo) -> Result<Response<Body>, io::Error>{
+    let cookie = req_info
+    .headers()
+    .get(header::COOKIE)
+    .and_then(|v| v.to_str().ok())
+    .unwrap_or("");
+   let id = cookie.split("=").collect::<Vec<&str>>();
+   if id[1] == "admin" {
+    return Ok(res);
+   }
+   Ok(Response::new(Body::from("not allow!")))
 }
 
 fn router() -> Router<Body, io::Error> {
-   Router::builder().middleware(Middleware::pre(logger_middleware))
-   .get("/", index)
-   .get("/blog", blog)
-   .get("/blog/:id", blog)
-   .build()
-   .unwrap()
+    Router::builder()
+        .middleware(Middleware::pre(logger_middleware))
+        .get("/", index)
+        .get("/login",login)
+        .get("/blog", blog)
+        .get("/blog/:id", blog)
+        .scope("/api", api::router())
+        .build()
+        .unwrap()
 }
 
+
+mod api {
+    use super::*;
+
+
+
+    pub fn router() -> Router<Body, io::Error> {
+        // Create a router for API and specify the the handlers.
+        Router::builder().middleware(Middleware::post_with_info(protect))
+            .get("/admin", protect_page)
+            .build()
+            .unwrap()
+    }
+}
